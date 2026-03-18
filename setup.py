ef2b33c5c8e8ae6c18dfe6bce1d4fb7eae2bccfe
Scottcandy34 <bzbrandonz@gmail.com>
2026-03-18 10:52:45 -0400

    Initializes Python package for distribution
    Sets up `create3` as an installable Python package.
    Defines essential package metadata, including description, author, and MIT license.
    Specifies core ROS2 message type dependencies for iRobot Create3 control.
    Implements dynamic versioning with a build timestamp for development releases.

---
+++ setup.py
@@ -0,0 +1,38 @@
+import datetime
+
+from setuptools import setup, find_packages
+
+readme = open("README.md").read()
+
+fp = open("create3/__version__.py", "r").read()
+VERSION = eval(fp.strip().split()[-1])
+
+requirements = [
+    "irobot_create_msgs",
+    "rclpy",
+    "geometry_msgs",
+    "sensor_msgs",
+    "nav_msgs",
+    "builtin_interfaces",
+    "std_msgs",
+]
+
+VERSION += "_" + datetime.datetime.now().strftime("%Y%m%d%H%M")[2:]
+
+setup(
+    name="create3",
+    version=VERSION,
+    author="Scottcandy34",
+    url="https://github.com/scottcandy34/irobot-create3-python-sdk/",
+    package_dir={"":"create3"},
+    description="SDK for iRobot Create3 robot control over ROS2",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    licence="MIT",
+    packages=find_packages("create3"),
+    zip_safe=True,
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+)
